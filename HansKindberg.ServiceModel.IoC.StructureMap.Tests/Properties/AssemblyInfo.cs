3762f62b17d350e9dd272ca0323b2cf222d87f02
Hans Kindberg <hans.kindberg@gmail.com>
2012-04-22 15:08:19 +0200

    Marked all libraries CLS-compliant except "HansKindberg.ServiceModel.IoC.StructureMap". StructureMap is not marked CLS-compliant.

--- HansKindberg.ServiceModel.IoC.StructureMap.Tests/Properties/AssemblyInfo.cs
+++ HansKindberg.ServiceModel.IoC.StructureMap.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("HansKindberg.ServiceModel.IoC.StructureMap.Tests")]
 [assembly: AssemblyProduct("HansKindberg.ServiceModel.IoC.StructureMap.Tests")]
 [assembly: AssemblyDescription("Tests for HansKindberg.ServiceModel.IoC.StructureMap assembly")]
+[assembly: CLSCompliant(true)]
 [assembly: Guid("c6eda055-8ac0-4eaf-8ae2-e1e45af5b3de")]
